0d6e9e2466adf297d2869e08e00ec618543190ed
SoghomonyanElina <elinasogh@DESKTOP-VPQOFK7>
2024-10-21 01:47:45 +0400

    Problem1464/MaximumProductofTwoElements

---
+++ Homeworks/problem_1464.cpp
@@ -0,0 +1,14 @@
+#include <queue>
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        priority_queue<int> maxHeap;
+        for(int i = 0; i < nums.size(); ++i) {
+            maxHeap.push(nums[i]);
+        }
+        int max1 = maxHeap.top();
+        maxHeap.pop();
+        int max2 = maxHeap.top();
+        return (max1 - 1) * (max2 - 1);
+    }
+};
